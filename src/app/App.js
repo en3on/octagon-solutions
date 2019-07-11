44a6a469793965982a88b6921ce7bdf76f7d3ae0
praveen <praveen.mut@gmail.com>
2019-07-12 01:14:06 +1000

    remove redundant divs in App.js render method

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
 import Routes from './Routes'
-import ContactForm from '../contact/ContactForm';
-import SignInForm from '../user/SignInForm';
-import RegisterForm from '../user/RegisterForm';
 
 class App extends Component {
   render() {
     return (
-    <div>
       <Routes />
-    </div>
     );
   };
 }
