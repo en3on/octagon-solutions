5ebd9e6512ebcb56c0217cb09f5aee7db5fb1a26
praveen <praveen.mut@gmail.com>
2019-07-12 00:40:01 +1000

    add routes to App.js and create basic structure for Routes.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import Routes from './Routes'
 import ContactForm from '../contact/ContactForm';
 import SignInForm from '../user/SignInForm';
 import RegisterForm from '../user/RegisterForm';
@@ -8,7 +9,7 @@ class App extends Component {
   render() {
     return (
     <div>
-
+      <Routes />
     </div>
     );
   };
---
+++ src/app/routes.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+
+class Routes extends Component {
+
+}
+
+export default Routes;
