92bf0be7a4b42e50a343e9514f9eafe69f790d3a
praveen <praveen.mut@gmail.com>
2019-07-12 00:56:18 +1000

    populate routes in Routes.js

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,8 +1,23 @@
 import React, {Component} from 'react';
 import {Switch, Route, Link} from 'react-router-dom';
+import ContactForm from '../contact/ContactForm';
+import SignInForm from '../user/SignInForm';
+import RegisterForm from '../user/RegisterForm';
+
 
 class Routes extends Component {
-
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/signin" component={SignInForm} />
+        <Route path="/register" component={RegisterForm} />
+        <Route path="/admin/user/:id" component={UserProfile} />
+        <Route path="/user/:id" component={DashBoard} />
+        <Route component={PageNotFound} />
+      </Switch>
+    )
+  }
 }
 
 export default Routes;
