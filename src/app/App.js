5c9b6dc832a2e6f8a2449ef37b7dce44533755c7
praveen <praveen.mut@gmail.com>
2019-07-12 15:37:09 +1000

    Complete Package Card

--- src/app/App.js
+++ src/app/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import ContactForm from '../contact/ContactForm';
 import SignInForm from '../user/SignInForm';
 import RegisterForm from '../user/RegisterForm';
+import PackageCard from '../packages/PackageCard';
 
 class App extends Component {
   render() {
     return (
     <div>
-
+      <PackageCard />
     </div>
     );
   };
---
+++ src/packages/PackageCard.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {Card, Button} from 'react-bootstrap';
+import './PackageCard.css'
+
+class PackageCard extends Component {
+  render() {
+    return (
+      <Card className="package-card">l
+        <Card.Img variant="top" src={this.props.img} />
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>
+          {this.props.text}
+          </Card.Text>
+          <Card.Text>
+            Price: ${this.props.price}
+          </Card.Text>
+          <Button variant="info">More Information</Button>
+      </Card.Body>
+      </Card>
+    );
+  }
+}
+
+export default PackageCard;
