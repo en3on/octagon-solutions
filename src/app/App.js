a0f21d8534d559f1e3a44455df1408e78babe4f9
praveen <praveen.mut@gmail.com>
2019-07-13 15:14:46 +1000

    add this bindings to file upload form

--- src/app/App.js
+++ src/app/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import ContactForm from '../contact/ContactForm';
 import SignInForm from '../user/SignInForm';
 import RegisterForm from '../user/RegisterForm';
+import FileUpload from '../fileUpload/FileUploadForm';
 
 class App extends Component {
   render() {
     return (
     <div>
-
+      <FileUpload />
     </div>
     );
   };
---
+++ src/fileUpload/FileUploadForm.js
@@ -0,0 +1,54 @@
+// File Upload for clients to file the administrator.
+// The File Upload requests First Name, Last Name & Email & message
+
+import React from 'react';
+import {Form, Button} from 'react-bootstrap';
+import './FileUpload.css';
+
+
+class FileUpload extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.submitHandler = this.submitHandler.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
+  
+  }
+
+  submitHandler(e) {
+    e.preventDefault();
+    // post data into db
+  }
+
+  handleFormChange(e) {
+    const {target} = e;
+
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="form-component-container">
+        <Form className="file-upload-form" onSubmit={this.submitHandler}>
+          <Form.Group controlId="formFileUpload">
+            <Form.Label>Upload Your Files</Form.Label>
+            <Form.Control type="file" /> 
+          </Form.Group>
+          <Form.Group controlId="formDescription">
+            <Form.Label>Description</Form.Label>
+            <Form.Control as="textarea" rows="5" name="description" onChange={this.handleFormChange} />
+          </Form.Group>
+          <Button variant="primary" id="fileSubmitButton" type="submit" onClick={this.submitHandler}>
+            Submit
+          </Button>
+        </Form>
+      </div>
+    )
+  }
+} 
+
+export default FileUpload;
