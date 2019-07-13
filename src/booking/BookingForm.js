fcbabed33d63af9f7de8b5016ced04403d005ea7
praveen <praveen.mut@gmail.com>
2019-07-14 02:45:06 +1000

    Finish up booking form

--- src/booking/BookingForm.js
+++ src/booking/BookingForm.js
@@ -10,15 +10,19 @@ class BookingForm extends React.Component {
     super(props);
     // assuming that the value of the available booking times are structured in an array format
     // retrieved from the API (i.e: ['Monday at 4:00PM', 'Tuesday at 3:00PM'])
-    this.state = {availableTimes: ['Monday at 4:00PM', 'Tuesday at 3:00PM']};  
+    this.state = {
+      availableTimes: ['Monday at 4:00PM', 'Tuesday at 3:00PM', 'Thursday at 3:00PM'], 
+      selectedTime: '',
+    };  
 
     this.submitHandler = this.submitHandler.bind(this);
     this.handleFormChange = this.handleFormChange.bind(this);
-    this.resetForm = this.resetForm.bind(this);
   }
 
   submitHandler(e) {
     e.preventDefault();
+    console.log(this.state.selectedTime);
+    console.log(this.state.message);
   }
 
   handleFormChange(e) {
@@ -29,24 +33,13 @@ class BookingForm extends React.Component {
     });
   }
 
-  resetForm(e) {
-    e.preventDefault();
-
-    this.setState({
-      firstName: '',
-      lastName: '',
-      email: '',
-      messsage: '',
-    })
-  }
-
   render() {
     return (
       <div className="form-component-container">
         <Form className="booking-form" onSubmit={this.submitHandler}>
           <Form.Group controlId="formBookingTime">
             <Form.Label>Available Times to Book:</Form.Label>
-            <Form.Control as="select" value={this.state.availableTimes} onChange={this.handleFormChange}>
+            <Form.Control as="select" name="selectedTime" value={this.state.selectedTime} onChange={this.handleFormChange}>
               {this.state.availableTimes.map((avaliableTime, idx) => {
                 return (
                 <option key={idx} value={avaliableTime}>{avaliableTime}</option>
@@ -55,7 +48,7 @@ class BookingForm extends React.Component {
             </Form.Control>
           </Form.Group>
           <Form.Group controlId="formMessage">
-            <Form.Label>Further Description (Any further questions or comments):</Form.Label>
+            <Form.Label>Further Description (any further questions or comments):</Form.Label>
             <Form.Control as="textarea" rows="5" name="message" onChange={this.handleFormChange} />
           </Form.Group>
           <Button variant="primary" id="BookingSubmitButton" type="submit" onClick={this.submitHandler}>
