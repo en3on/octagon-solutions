48b2b6e72d8340d1319795d79525189935731d10
praveen <praveen.mut@gmail.com>
2019-07-14 03:02:07 +1000

    clean App.js component insertions for inserting and imports

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
-import BookingForm from '../booking/BookingForm';
 
 class App extends Component {
   render() {
     return (
     <div>
-      <BookingForm />
+
     </div>
     );
   };
