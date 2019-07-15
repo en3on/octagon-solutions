50dcff17fb726af6fef020bc1604347384460f7f
praveen <praveen.mut@gmail.com>
2019-07-15 16:21:30 +1000

    add  admin routes

--- src/app/AdminRoutes.js
+++ src/app/AdminRoutes.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
+import AdminDashBoard from '../admin/AdminDashboard';
+import UserProfileView from '../admin/UserProfileView';
 
 class AdminRoutes extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <>
+        <Route path="/admin" component={AdminDashBoard} />
+        <Route exact path="/admin/user/:id" component={UserProfileView} />
+      </>
     );
   }
 };
