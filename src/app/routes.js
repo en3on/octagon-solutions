91bdf01862801a5a1050388b9aeb8f9d7739c227
praveen <praveen.mut@gmail.com>
2019-07-15 16:09:11 +1000

    add import statements into routes.js main file

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
 import {Switch} from 'react-router-dom';
+import RootRoutes from './RootRoutes';
+import UserRoutes from './UserRoutes';
+import AdminRoutes from './AdminRoutes';
+import BookingRoutes from './BookingRoutes';
+import ErrorRoutes from './ErrorRoutes';
+
 
 class Routes extends Component {
   render() {
