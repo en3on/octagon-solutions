a3bb14cbbc9a079f8ae0a6d7a51a3587903854d6
praveen <praveen.mut@gmail.com>
2019-07-15 16:25:13 +1000

    add error routes

---
+++ src/app/ErrorRoutes.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
+import PageNotFound from '../app/PageNotFound';
+
+class ErrorRoutes extends Component {
+  render() {
+    return (
+      <Route component={PageNotFound} />
+    );
+  }
+};
+
+export default ErrorRoutes;
--- src/app/routes.js
+++ src/app/routes.js
@@ -3,7 +3,7 @@ import {Switch} from 'react-router-dom';
 import RootRoutes from './RootRoutes';
 import UserRoutes from './UserRoutes';
 import AdminRoutes from './AdminRoutes';
-import BookingRoutes from './BookingRoutes';
+// import BookingRoutes from './BookingRoutes';
 import ErrorRoutes from './ErrorRoutes';
 
 
@@ -14,7 +14,7 @@ class Routes extends Component {
         <RootRoutes />
         <UserRoutes />
         <AdminRoutes />
-        <BookingRoutes />
+        {/* <BookingRoutes /> */}
         <ErrorRoutes />
       </Switch>
     )
