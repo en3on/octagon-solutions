ce63c4706138ea3e3fa90379d7bf48f7cba2f46d
praveen <praveen.mut@gmail.com>
2019-07-15 11:45:35 +1000

    remove FileUpload component from app.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import FileUpload from '../fileUpload/FileUploadForm';
 
 
 class App extends Component {
   render() {
     return (
     <div>
-      <FileUpload />
+      
     </div>
     );
   };
