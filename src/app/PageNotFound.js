17f84afb0697161919dd83e0cc40af77562f0d73
Adam Ladell <adamladell@outlook.com>
2019-07-16 12:00:02 +1000

    Change Double Quotes to Single for <Link>
    Changed to stay consistent with style guide

--- src/app/PageNotFound.js
+++ src/app/PageNotFound.js
@@ -5,7 +5,7 @@ function PageNotFound(props) {
   return (
     <div>
     <h1>The Requested Page cannot be found</h1>
-    <Link to="/">Return back home</Link>
+    <Link to='/'>Return back home</Link>
     </div>
   );
 }
