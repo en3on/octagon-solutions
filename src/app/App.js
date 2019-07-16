05f7f5abf292da3dcda44e04fa96514e9a46b9ba
praveen <praveen.mut@gmail.com>
2019-07-16 14:48:12 +1000

    clean up fileuploadform component

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
-import FileUploadForm from '../fileUpload/FileUploadForm';
 
 class App extends Component {
   render() {
     return (
     <div>
-      <FileUploadForm />
+
     </div>
     );
   };
