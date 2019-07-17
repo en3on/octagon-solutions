3668d088284355109f8850ae967e8e5742e9eebb
praveen <praveen.mut@gmail.com>
2019-07-17 11:40:33 +1000

    remove registration component

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import RegisterForm from '../user/RegisterForm';
 
 
 class App extends Component {
   render() {
     return (
     <div>
-      <RegisterForm />
+      
     </div>
     );
   };
