5d3a0e285baca859ed9df4af8cbcbbc6eb92525a
praveen <praveen.mut@gmail.com>
2019-07-18 15:01:15 +1000

    replaced localhost with .env

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
+import SignInForm from '../user/SignInForm';
 
 class App extends Component {
   render() {
     return (
     <div>
-      
+      <SignInForm />
     </div>
     );
   };
