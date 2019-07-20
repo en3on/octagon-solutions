12988686f724579efa7720f417220605edc0616d
praveen <praveen.mut@gmail.com>
2019-07-20 14:49:15 +1000

    add initial structure to the PasswordResetForm

---
+++ src/user/PasswordResetForm.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {Form, Button, Alert} from 'react-bootstrap';
+import './RegisterForm.css';
+
+
+class PasswordResetForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+    this.submitHandler = this.submitHandler.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
+  }
+
+  submitHandler(e) {
+
+  }
+
+  handleFormChange(e) {
+
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default PasswordResetForm;
