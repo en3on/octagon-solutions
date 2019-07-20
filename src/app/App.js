9313ee6197b7046dabb99945087793c882fa921d
praveen <praveen.mut@gmail.com>
2019-07-20 15:50:12 +1000

    clear PasswordResetForm component from App.js used for testing

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
-import PasswordResetForm from '../user/PasswordResetForm';
 
 
 class App extends Component {
   render() {
     return (
     <div>
-      <PasswordResetForm />
     </div>
     );
   };
