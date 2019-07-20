1eb12db4a77fff80269853e78db1661e85770c23
praveen <praveen.mut@gmail.com>
2019-07-20 16:03:25 +1000

    create constructor

--- src/user/PasswordResetRequestForm.js
+++ src/user/PasswordResetRequestForm.js
@@ -2,7 +2,11 @@ import React, {Component} from 'react';
 import {Form, Alert} from 'react-bootstrap';
 
 class PasswordResetRequestForm extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {};
+  }
 }
 
 export default PasswordResetRequestForm;
