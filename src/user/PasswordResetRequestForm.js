19d187ae02d72636ed64a432e9b1f0d7157a929a
praveen <praveen.mut@gmail.com>
2019-07-20 16:02:26 +1000

    Create File and initial skeleton for form

---
+++ src/user/PasswordResetRequestForm.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {Form, Alert} from 'react-bootstrap';
+
+class PasswordResetRequestForm extends Component {
+
+}
+
+export default PasswordResetRequestForm;
