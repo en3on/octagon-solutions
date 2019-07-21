50c23a3d33c914df91225839d84ad02e8568e14b
praveen <praveen.mut@gmail.com>
2019-07-21 15:56:40 +1000

    remove components from the main app.js page used for testing

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
-import RegisterPage from '../user/RegisterPage';
-import UserEditPage from '../user/UserEditPage';
 
 
 class App extends Component {
   render() {
     return (
     <div>
-      <RegisterPage />
     </div>
     );
   };
