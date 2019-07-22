9e26fd8d9d01dd727a312cadffe4b71c1767a525
praveen <praveen.mut@gmail.com>
2019-07-22 16:52:28 +1000

    add Password Reset From to App.js to visually test

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
+import PasswordResetForm from '../user/PasswordResetForm';
 
 
 class App extends Component {
   render() {
     return (
     <div>
+      <PasswordResetForm />
     </div>
     );
   };
