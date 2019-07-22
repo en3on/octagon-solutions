fc8598e3df184d8ca40b20fbaffec917c4129a11
praveen <praveen.mut@gmail.com>
2019-07-22 16:45:03 +1000

    remove the Password Reset Request Form from App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import PasswordResetRequestForm from '../user/PasswordResetRequestForm';
 
 
 class App extends Component {
   render() {
     return (
     <div>
-      <PasswordResetRequestForm />
+
     </div>
     );
   };
