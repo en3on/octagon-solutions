178ec558c768dbdab3c458d48409a237f5b444f3
praveen <praveen.mut@gmail.com>
2019-07-24 00:07:59 +1000

    finish up footer

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Routes from './Routes';
+import Footer from './Footer';
 
 class App extends Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class App extends Component {
     return (
       <div>
         <Routes authToken={auth} />
+        <Footer />
       </div>
     );
   };
