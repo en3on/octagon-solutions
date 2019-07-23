7f90f2c1c9aa8588ce9ca233a6021b383e064bd8
praveen <praveen.mut@gmail.com>
2019-07-23 21:43:16 +1000

    create initial skeleton for Navbar

---
+++ src/app/Navbar.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {Navbar} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+
+class Navbar extends Component {
+  render() {
+
+  }
+}
+
+export default Navbar;
