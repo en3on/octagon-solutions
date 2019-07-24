3ee71ecfded20598bc805f903340598c541ad554
praveen <praveen.mut@gmail.com>
2019-07-24 12:23:08 +1000

    remove descriptions

--- src/fileUpload/FileUploadForm.js
+++ src/fileUpload/FileUploadForm.js
@@ -19,61 +19,48 @@ class FileUpload extends Component {
 
     this.submitHandler = this.submitHandler.bind(this);
     this.handleUploadFormChange = this.handleUploadFormChange.bind(this); 
-    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
-    this.descriptionChecker = this.descriptionChecker.bind(this);
-    this.formDataHandler = this.formDataHandler.bind(this);
     this.fileUploader = this.fileUploader.bind(this);
   }
 
   handleUploadFormChange(e) {
-    console.log({e});
     this.setState({
-      files: [...this.state.files, {file: e.target.files[0], description: ''}],
+      files: [...this.state.files, e.target.files[0]],
       filenames: [...this.state.filenames, e.target.files[0].name],
       errorMessage: '',
     });
-    console.log(this.state);
     e.target.value = '';
   }
 
-  handleDescriptionChange(e) {
-    const files = this.state.files.slice();
-    files[e.target.name].description = e.target.value;
-    this.setState({
-      files,
-      errorMessage: '',
-    });
-  }
-
-  descriptionChecker(ary) {
-    for(let e = 0; e < ary.length; e++) {
-      if(ary[e].description === '') {
-        return false;
-      }
-    }
-    return true;
-  }
+  // handleDescriptionChange(e) {
+  //   const files = this.state.files.slice();
+  //   files[e.target.name].description = e.target.value;
+  //   this.setState({
+  //     files,
+  //     errorMessage: '',
+  //   });
+  // }
 
   submitHandler(e) {
     e.preventDefault();
-    if(this.state.files.length !== 0 && this.descriptionChecker(this.state.files)) {
+    if(this.state.files.length !== 0) {
       console.log('submitted');
-      this.setState({inSumbit: true});
+      this.setState({inSubmit: true});
     } else {
-      this.setState({errorMessage: "Please upload your files and enter a description"});
+      this.setState({errorMessage: "Please upload your files"});
       console.log('not submitted');
       return
     };
     const filesAry = this.state.files;
-    this.formDataHandler(filesAry);
+    const data = {
+      files: filesAry
+    };
+    console.log(data);
+    this.fileUploader(data);
   }
 
-  formDataHandler(filesAry) {
-    this.fileUploader(filesAry);
-  }
 
   async fileUploader(payload) {
-    console.log({payload});
+    console.log(payload);
     try {
     //   const response = await axios({
     //     method: 'post',
@@ -89,7 +76,6 @@ class FileUpload extends Component {
           'Content-Type': 'application/x-www-form-urlencoded',
         },
       });
-
       const {status: responseStatus} = response; 
 
       this.setState({responseStatus});
@@ -111,8 +97,8 @@ class FileUpload extends Component {
     return (
       <div className="form-component-container">
         <Form className="file-upload-form">
-        <div id="fie-uploading">
-          {this.state.inSumbit && <Alert variant="success">Uploading Files. Please Wait.</Alert>}
+        <div id="file-uploading">
+          {this.state.inSubmit && <Alert variant="success">Uploading Files. Please Wait.</Alert>}
         </div>  
         <div id="file-not-found">
           {this.state.errorMessage && <Alert variant="danger">{this.state.errorMessage}</Alert>}
@@ -124,18 +110,14 @@ class FileUpload extends Component {
           <div id="uploadedFilesLabel">
             <span>Your Uploaded Files:</span>
           </div>
-          {this.state.files.map(({file}, idx) => {
+          {this.state.files.map((file) => {
             return (
               <ListGroup key={file.name}>
                 <ListGroup.Item className="itemUpload" variant="dark" key={file.name}>{file.name}</ListGroup.Item>
-                <Form.Group className="my-2" controlId="formDescription">
-                  <Form.Label>{`Provide a Description of ${file.name}:`}</Form.Label>
-                  <Form.Control as="textarea" rows="3" onChange={this.handleDescriptionChange} name={idx} />
-                </Form.Group>
               </ListGroup>
             );
           })}
-          <Button variant="primary" id="fileSubmitButton" type="submit" onClick={this.submitHandler} disabled={this.state.inSumbit}>
+          <Button className="my-2" variant="primary" id="fileSubmitButton" type="submit" onClick={this.submitHandler} disabled={this.state.insubmit}>
             Submit
           </Button>
         </Form>
