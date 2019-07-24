06902a678e4dca4b563ff2b9b21981199d8a8f9b
praveen <praveen.mut@gmail.com>
2019-07-24 15:34:34 +1000

    work on the home page initial jumbotron styles

--- src/landing/Home.js
+++ src/landing/Home.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import {Jumbotron, Button} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import './Home.css';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        Hello from Root/Index Page!
+      <Jumbotron>
+        <h1>Welcome to Octogon Accounting Solutions!</h1>
+        <p>Octagon Bookkeeping Solutions is a professional organisation that provides comprehensive bookkeeping and administrative services for small businesses.</p>
+        <p><Button variant="primary"><Link to="/about">About Us!</Link></Button></p>
+      </Jumbotron>
       </div>
     )
   }
