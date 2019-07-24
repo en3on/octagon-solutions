17e3671dee9f75329c6fa7a71d4ce12ae4b87de5
praveen <praveen.mut@gmail.com>
2019-07-24 14:35:43 +1000

    fix up about page

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Home from '../landing/Home';
-import About from '../about/AboutPage';
+import AboutPage from '../about/AboutPage';
 import SignInPage from '../user/SignInPage';
 import ContactForm from '../contact/ContactForm';
 import RegisterPage from '../user/RegisterPage';
 import PageNotFound from './PageNotFound';
 import ProtectedRoutes from './ProtectedRoutes';
-import AboutPage from '../about/AboutPage';
-
 
 class Routes extends Component {
 
